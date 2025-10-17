ea57274d73c450b3dabfe217b9b14138222b2b4c
Avrijalajah <avrijal190409@student.abudzar.sch.id>
2025-10-17 11:23:35 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
+import New from "./components/New/New";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Hero />
       <About />
+      <New />
     </>
   );
 }
