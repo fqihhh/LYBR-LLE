9ac8daea51ebbe830ab8a2a702239c3cdad685a8
Fathi-Dz <fathi0504@student.abudzar.sch.id>
2025-10-21 13:44:18 +0700

    add

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,16 @@ import About from "./components/About/About";
 import New from "./components/New/New";
 import Footer from "./components/Footer/Footer";
 import Featured from "./components/Featured/Featured";
-import Find from "./components/Find/Find";
+// import Find from "./components/Find/Find";
 
 function App() {
-  const [showSetup, setShowSetup] = useState(false);
-
   return (
     <>
-      {/* Header tetap muncul */}
-      <Header />
-
-      {/* Kalau setup aktif, section lain hide */}
-      <div className={`transition-all duration-700 ${showSetup ? "hidden" : "block"}`}>
-        <Hero />
-        <New />
-        <Featured />
-        <About />
-        
-      </div>
-
-      {/* Find selalu render tapi punya mode sendiri */}
-      <div
-        className={`transition-all duration-700 ${
-          showSetup ? "mt-0" : "mt-[150vh]"
-        }`}
-      >
-        <Find showSetup={showSetup} setShowSetup={setShowSetup} />
-      </div>
+      <Header  />
+      <Hero />
+      <About />
+      <New />
+      <Featured />
       <Footer />
     </>
   );
