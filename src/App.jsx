3c333e702f6836091136821ec8c1fbe3fd643197
Avrijalajah <avrijal190409@student.abudzar.sch.id>
2025-10-31 10:30:22 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,7 @@ import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import New from "./components/New/New";
 import Featured from "./components/Featured/Featured";
-// import Products from "./components/Products/Products";
-
 import Sindir from "./components/Sindir/Sindir";
-
 import Footer from "./components/Footer/Footer";
 import Find from "./components/Find/Find";
 
@@ -20,7 +17,6 @@ function App() {
       <Hero />
       <New />
       <Featured />
-      {/* <Products /> */}
       <About />
       <Find />
       <Sindir />
@@ -30,40 +26,3 @@ function App() {
 }
 
 export default App;
-
-
-// import { useState } from "react";
-// import Header from "./components/Header/Header";
-// import Hero from "./components/Hero/Hero";
-// import About from "./components/About/About";
-// import New from "./components/New/New";
-// import Featured from "./components/Featured/Featured";
-// import Footer from "./components/Footer/Footer";
-// import Products from "./components/Products/Products";
-
-// function App() {
-//   const [page, setPage] = useState("home");
-
-//   return (
-//     <>
-//       <Header setPage={setPage} />
-
-//       {page === "home" && (
-//         <>
-//           <Hero />
-//           <About />
-//           <New />
-//           <Featured />
-//         </>
-//       )}
-
-//       {page === "products" && <Products />}
-
-//       <Footer />
-//     </>
-//   );
-// }
-
-// export default App;
-
-
